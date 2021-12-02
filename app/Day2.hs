d5c486d60a0f23d8d6540c8b9088df015da3b59d
heitor <heitor@bluebaron.local>
2021-12-02 11:19:38 +0000

    Day 2

---
+++ app/Day2.hs
@@ -0,0 +1,65 @@
+module Day2 where
+
+
+import Control.Monad
+import Data.List
+
+
+data Action = Forward Int | Up Int | Down Int deriving(Show)
+
+
+splitLine :: String -> [String]
+splitLine "" = []
+splitLine s = a:splitLine (dropWhile (==' ') b)
+    where (a, b) = span (/=' ') s
+
+    
+parseLine :: String -> Either String Action
+parseLine s = case splitLine s of
+    ["up", x]      -> Right $ Up (read x)
+    ["down", x]    -> Right $ Down (read x)
+    ["forward", x] -> Right $ Forward (read x)
+    _              -> Left $ "Invalid command: " ++ s
+
+
+parseLines :: String -> Either String [Action]
+parseLines = sequence . (map parseLine) . lines
+
+
+class Submarine s where
+    moveSubmarine :: s -> Action -> s
+    position :: s -> (Int, Int)
+
+
+data ShittySubmarine = ShittySubmarine Int Int deriving(Show)
+instance Submarine ShittySubmarine where
+    moveSubmarine (ShittySubmarine x y) (Forward i) = ShittySubmarine (x + i) y
+    moveSubmarine (ShittySubmarine x y) (Up i) = ShittySubmarine x (y + i)
+    moveSubmarine (ShittySubmarine x y) (Down i) = ShittySubmarine x (y - i)
+    position (ShittySubmarine x y) = (x, y)
+
+
+data AdequateSubmarine = AdequateSubmarine Int Int Int deriving(Show)
+instance Submarine AdequateSubmarine where
+    moveSubmarine (AdequateSubmarine aim x y) (Forward i) = AdequateSubmarine aim (x + i) (y + aim * i)
+    moveSubmarine (AdequateSubmarine aim x y) (Up i) = AdequateSubmarine (aim + i) x y
+    moveSubmarine (AdequateSubmarine aim x y) (Down i) = AdequateSubmarine (aim - i) x y
+    position (AdequateSubmarine aim x y) = (x, y)
+
+
+result :: Submarine s => s -> Int
+result = op . position
+    where op (x, y) = x * (-y)
+
+
+main :: IO ()
+main = do
+    actions <- parseLines <$!> readFile "data/day2.txt"
+
+    let shittySub = foldl moveSubmarine (ShittySubmarine 0 0) <$> actions
+    putStr "Shitty submarine: "
+    putStrLn $ show $ result <$> shittySub
+
+    let betterSub = foldl moveSubmarine (AdequateSubmarine 0 0 0) <$> actions
+    putStr "Better submarine: "
+    putStrLn $ show $ result <$> betterSub
--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,12 @@ module Main where
 
 import System.Environment
 import qualified Day1
+import qualified Day2
 
 
 runDay :: [String] -> IO ()
 runDay ["1"] = Day1.main
+runDay ["2"] = Day2.main
 runDay []    = putStrLn "Tell me a day to run."
 runDay _     = putStrLn "I've no idea what you're talking about."
 
